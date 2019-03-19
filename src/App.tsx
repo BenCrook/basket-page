54dbf919d8fdd12a57888c0167cdd705046ded5c
Ben Crook <ben@space48.com>
2019-03-19 18:51:51 +0000

    * Tidy up logging on successful checkout

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,12 @@ import ProductGrid from "./components/product-grid/ProductGrid";
 import ProgressSteps from "./components/progress-steps/ProgressSteps";
 import { initialBasket } from "./data";
 import Totals from "./components/totals/Totals";
+import { BasketContentsInterface } from "./types";
+
+const successfulCheckout = (basket: BasketContentsInterface[]) => {
+    console.log(basket);
+    console.log('You have successfully checked out!');
+};
 
 const App = () => {
     const [basket, updateBasket] = useState(initialBasket);
@@ -12,7 +18,7 @@ const App = () => {
         <>
             <ProductGrid basket={basket} updateBasket={updateBasket} />
             <Totals basket={basket} />
-            <button disabled={!basket.length} onClick={() => console.log(basket)}>Checkout</button>
+            <button disabled={!basket.length} onClick={() => successfulCheckout(basket)}>Checkout</button>
         </>
     );
 
